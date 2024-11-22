708b47ff3476edaf09d7de6ca776d0ae8927675f
karivarkey <geevargheseregi78@gmail.com>
2024-11-22 08:53:20 +0530

    Revert "Update gitprofile.config.js"
    This reverts commit 80090b14e1618205b9e3646e89abafc4008d9e74.

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -2,7 +2,7 @@
 
 const config = {
   github: {
-    username: 'karivarkey', // Your GitHub org/user name. (Required)
+    username: 'KARI-VARKEY', // Your GitHub org/user name. (Required)
     sortBy: 'stars', // stars | updated
     limit: 10, // How many projects to display.
     exclude: {
