0bd4e8ebb9efb6043a20522be85b7c8795dd6d0b
Geevarghese Regi <79575529+karivarkey@users.noreply.github.com>
2024-11-27 09:11:58 +0530

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -2,7 +2,7 @@
 
 const config = {
   github: {
-    username: 'KARI-VARKEY', // Your GitHub org/user name. (Required)
+    username: 'karivarkey', // Your GitHub org/user name. (Required)
     sortBy: 'stars', // stars | updated
     limit: 10, // How many projects to display.
     exclude: {
